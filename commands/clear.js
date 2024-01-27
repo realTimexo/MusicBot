c69dd25f65f40cd5140a28d1ad3e43559594364e
BrK08 <126277787+BrK08@users.noreply.github.com>
2024-01-27 14:18:39 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -17,8 +17,8 @@ const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  name: "clear",
-  description: "Clears the music queue.",
+  name: "Temizle",
+  description: "Kuyruktaki şarkıları temizler.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -27,11 +27,11 @@ module.exports = {
     
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Şuan müzik çalmıyor', ephemeral: true });
       }
 
       if (!queue.songs[0]) {
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true });
+        return interaction.reply({ content: '❌ Kuyruk Boş!!', ephemeral: true });
       }
 
       await queue.stop(interaction.guild.id);
@@ -43,7 +43,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157314241393598585/4618-no-slides.png?ex=65182861&is=6516d6e1&hm=dac8fed5a18e1574485e833d4c017591c50f59d161e1bde7fed5f6a92543f951&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**Queue cleared! Be Ready for a new musical journey.**')
+        .setDescription('**Kuyruk temizlendi! Yeni bir müzikal yolculuğa hazır olun.**')
        
 
       interaction.reply({ embeds: [embed] });
