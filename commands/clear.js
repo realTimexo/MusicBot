69e61a85763997bb1ffb66b6c6ddee2bd9649931
realTimexo <166215287+realTimexo@users.noreply.github.com>
2025-01-21 16:19:29 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -8,17 +8,18 @@
   ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
 
    
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   # ERSTELLT VON RTX!! KANN FREI VERWENDET WERDEN
+   ## FÜR HILFE KONTAKTIERE MICH AUF DISCORD
+   ## Kontakt    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
    ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
 */
+
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  name: "Temizle",
-  description: "Kuyruktaki şarkıları temizler.",
+  name: "clear",
+  description: "Clears the songs in the queue.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -27,11 +28,11 @@ module.exports = {
     
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ Şuan müzik çalmıyor', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Es wird gerade keine Musik abgespielt.', ephemeral: true });
       }
 
       if (!queue.songs[0]) {
-        return interaction.reply({ content: '❌ Kuyruk Boş!!', ephemeral: true });
+        return interaction.reply({ content: '❌ Die Warteschlange ist leer!', ephemeral: true });
       }
 
       await queue.stop(interaction.guild.id);
@@ -39,11 +40,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#3498db')
         .setAuthor({
-          name: 'Cleared List',
+          name: 'Warteschlange geleert',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157314241393598585/4618-no-slides.png?ex=65182861&is=6516d6e1&hm=dac8fed5a18e1574485e833d4c017591c50f59d161e1bde7fed5f6a92543f951&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**Kuyruk temizlendi! Yeni bir müzikal yolculuğa hazır olun.**')
+        .setDescription('**Die Warteschlange wurde geleert! Bereit für eine neue musikalische Reise.**')
        
 
       interaction.reply({ embeds: [embed] });
@@ -63,8 +64,8 @@ module.exports = {
   ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
 
    
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
+   # ERSTELLT VON RTX!! KANN FREI VERWENDET WERDEN
+   ## FÜR HILFE KONTAKTIERE MICH AUF DISCORD
+   ## Kontakt    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
    ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
 */
